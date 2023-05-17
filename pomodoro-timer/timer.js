137e98ca6e76a3be3e119de1ee1d829c94eb8feb
Jorge Rivas <97417231+J0rgeR1vas@users.noreply.github.com>
2023-05-16 19:45:08 -0500

    Update timer.js

--- pomodoro-timer/timer.js
+++ pomodoro-timer/timer.js
@@ -1,23 +1,46 @@
-function setTimer() {
-    let minutes = 25;
-    let seconds = 0;
-    let interval = setInterval(() => {
-        seconds--;
-        if (seconds < 0) {
-            seconds = 59;
-            minutes--;
-        }
-        console.log(`${minutes}:${seconds < 10 ? "0" : ""}`);
-        if (minutes === 0 && seconds === 0) {
-            clearInterval(interval);
-            console.log("Time's up! Take break now for 5 minutes.");
-            //not working at this moment
-          setTimeout(() => {
-            console.log("Break is over. starting timer again.");
-            setTimer();
-          }, 5 * 60 * 1000);  
-        } 
-    }, 1000);
+let interval;
+
+function startTimer() {
+  if (interval) return; // Timer already running, ignore the click
+  
+  let minutes = 25;
+  let seconds = 0;
+  let timerElement = document.getElementById("timer");
+  let messageElement = document.getElementById("message");
+
+  interval = setInterval(() => {
+    seconds--;
+    if (seconds < 0) {
+      seconds = 59;
+      minutes--;
+    }
+    timerElement.textContent = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+
+    if (minutes === 0 && seconds === 0) {
+      clearInterval(interval);
+      interval = null;
+      messageElement.textContent = "Time's up! Take a break now for 5 minutes.";
+
+      setTimeout(() => {
+        messageElement.textContent = "Break is over. Starting timer again.";
+        startTimer();
+      }, 5 * 60 * 1000);
+    }
+  }, 1000);
 }
 
-setTimer();
+function resetTimer() {
+  if (interval) {
+    clearInterval(interval);
+    interval = null;
+  }
+
+  let timerElement = document.getElementById("timer");
+  let messageElement = document.getElementById("message");
+
+  timerElement.textContent = "25:00";
+  messageElement.textContent = "";
+}
+
+
+
